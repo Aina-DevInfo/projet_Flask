47cac3533d47fc7af322512464bed630d38e3206
Aina <rasolofomandimbyjhermana@gmail.com>
2025-07-13 19:49:29 +0300

    modification numero 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Solofo Ny Aina, André sy Faneva randrianatenaina"
+    return "RASOLOFOMANDIMBY Onitriniaina Jean hermana"
 
 if __name__ == '__main__':
     app.run()
